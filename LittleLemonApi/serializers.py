1b2fb051d907a32e0a882d012d1576fb139353bc
maqsoom <maqsoomahmed98@gmail.com>
2023-12-12 09:24:49 +0500

    serializer setting

--- LittleLemonApi/serializers.py
+++ LittleLemonApi/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from . models import MenueItem
+from decimal import Decimal
 
 class MenueItemSerializer(serializers.ModelSerializer):
+    stock = serializers.IntegerField(source= 'invetory')
+    discounted_price_ = serializers.SerializerMethodField(method_name='discounted_price')
     class Meta:
         model = MenueItem
-        fields = ['id','title','price','invetory']
+        fields = ['id','title','price','stock','discounted_price_']
+        
+    def discounted_price(self,product:MenueItem):
+        return product.price -((product.price*10)/100)
         
